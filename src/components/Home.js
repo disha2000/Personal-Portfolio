0533fb0a9bfcdd48dbe2bb94c74e129702bf5de7
Disha <doshidisha555@gmail.com>
2022-07-11 22:42:58 +0530

    home page 0.1v

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ import { resumeLink,socialLinks} from './data'
 export default function Home() {
   return (
     <div className='home-section'>
-     <div className='home-content'>
-     <div className="home_social">
+      <div className='home_grid'>
+          <div className="home_social">
             <div className ="social_icons" data-aos="fade-up-right">
                 <a href = {socialLinks[1].url} className = "icon"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a>
                 <a  className = "icon"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" strokeLinecap="round" className="feather feather-instagram"><title>Instagram</title><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg></a>
@@ -14,19 +14,18 @@ export default function Home() {
                 <a href = {socialLinks[0].url} className = "icon"><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" strokeLinecap="round" className="feather feather-linkedin"><title>LinkedIn</title><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
             </div>
         </div>
-        <div className="title"   data-aos="zoom-in-down">
-          <h3>Hello, I'am</h3>
-          <h1 >Disha Doshi</h1>
-          <div> <h2 >I am a Web Developer </h2> <h3>Think <span style = {{color:'cyan',fontSize:'35px'}}>.</span> Code <span style = {{color:'cyan',fontSize:'35px'}}>.</span> Debug</h3></div>
-          
-          
-          <a href={resumeLink}target="_blank">  <button className="custom-btn btn-4">Download CV </button></a> 
-        
+        <div className='home_content'>
+           <div className='home_title'>
+               <h3>Hello, I am</h3>
+              <h1> <br/> DISHA DOSHI.</h1>
+              <h2>A Web Developer.</h2>
+              <a href={resumeLink}target="_blank">  <button className="custom-btn btn-4">Download CV </button></a> 
+           </div> 
+           <div className='img-gradient'>
+
+           </div>
         </div>
-        <div className='img-graident'>
-         {/* <img src = {textanim} alt='animated text' className='textanim'></img> */}
-        </div>
-     </div>
+      </div>
     </div>
   )
 }
