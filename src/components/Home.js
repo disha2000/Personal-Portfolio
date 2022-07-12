91d05112a9a9488c2d5e62ea42f93ee2cd51e706
Disha <doshidisha555@gmail.com>
2022-07-12 14:24:13 +0530

    changed home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
            <div className='home_title'>
               <h3>Hi my name is,</h3>
               <h1>Disha Doshi.</h1>
-              <h4>A passionate Intermediate Software Engineer having experience in building responsive Web applications with JavaScript / Reactjs and some other cool libraries and frameworks.</h4>
+              <h4>A passionate Intermediate Software Engineer having experience in building <span className='highlight'>responsive web applications</span> with JavaScript / Reactjs and some other cool libraries and frameworks.</h4>
               <div className='home_btn'>
               <a href={resumeLink}target="_blank">  <button className="custom-btn btn-4">RESUME</button></a>
     
